355bb49b5ff5e8efdaf2f6aaa195674337d92109
Sam D <38329857+samdis@users.noreply.github.com>
2020-07-31 17:48:22 -0600

    Add text box and api get call

--- divicalreact/src/App.js
+++ divicalreact/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      ticker: '',
+      value: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit(){
+    axios.get('http://localhost:8080/dividends/getByTicker?ticker='+this.state.value)
+      .then(response => console.log(response.data.ticker))
+      //      .then(response => this.setState({ticker: response.data.ticker}))
+      console.log(this.state.ticker)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
 }
-
-export default App;
+export default App
