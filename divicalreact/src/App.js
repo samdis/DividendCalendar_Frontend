5b653c8ed49957f62d2031d10d77afecffd157b6
Sam D <38329857+samdis@users.noreply.github.com>
2020-08-01 17:41:31 -0600

    Adding in grid

--- divicalreact/src/App.js
+++ divicalreact/src/App.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react'
-import './App.css'
-import axios from 'axios'
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
       value: '',
-      ticker: ''
+      ticker: '',
+      date: new Date().toLocaleString(),
+      columnDefs: [{
+        headerName: "Ticker", field: "ticker"
+      }, {
+        headerName: "Date", field: "dates"
+      }],
+      rowData: ''
     }
 
     this.handleClick = this.handleClick.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
 
-  handleClick(){
+  handleClick(event){
     axios.get('http://localhost:8080/dividends/getByTicker?ticker=' + this.state.value)
-    .then(response => this.setState({ticker: response.data.ticker}))
-    alert(this.state.ticker)
+    .then(response => this.setState({rowData: response}))
+    event.preventDefault()
   }
 
   handleChange(event) {
@@ -32,7 +42,17 @@ class App extends Component {
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <button className='button' onClick={this.handleClick}>Click Me</button>
-        <p>{this.state.ticker}</p>
+        <div
+          className="ag-theme-alpine"
+          style={{
+          height: '250px',
+          width: '600px' }}
+          >
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}>
+        </AgGridReact>
+      </div>
       </form>
     )
   }
