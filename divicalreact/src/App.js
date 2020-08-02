50d24eb5cb119fa78c3320f401f2191ff9193012
Sam D <38329857+samdis@users.noreply.github.com>
2020-08-01 19:39:25 -0600

    Starting to integrate google calender api, currently not functional, will work on it soon

--- divicalreact/src/App.js
+++ divicalreact/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import ApiCalendar from 'react-google-calendar-api';
 
 class App extends Component {
   constructor(){
@@ -17,17 +18,37 @@ class App extends Component {
       }, {
         headerName: "Date", field: "dates"
       }],
-      rowData: ''
+      rowData: []
     }
 
-    this.handleClick = this.handleClick.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.addRowData = this.addRowData.bind(this);
+    //google cal
+    this.handleItemClick = this.handleItemClick.bind(this);
   }
 
-  handleClick(event){
+  handleClick(){
     axios.get('http://localhost:8080/dividends/getByTicker?ticker=' + this.state.value)
-    .then(response => this.setState({rowData: response}))
-    event.preventDefault()
+    .then(response => { console.log(response);
+                        //this.setState({rowData: [{ticker:response.data.ticker, dates:response.data.dates.toString()}]});
+                        this.addRowData(response);
+                      });
+  }
+
+  public handleItemClick(event: SyntheticEvent<any>, name: string): void {
+      if (name === 'sign-in') {
+        ApiCalendar.handleAuthClick();
+      } else if (name === 'sign-out') {
+        ApiCalendar.handleSignoutClick();
+      }
+
+  addRowData(response)
+  {
+    this.setState({
+      rowData : this.state.rowData.concat({ticker:response.data.ticker, dates:response.data.dates.toString()})
+    });
+    console.log(this.state.rowData);
   }
 
   handleChange(event) {
@@ -36,24 +57,34 @@ class App extends Component {
 
   render () {
     return (
-      <form>
+      <div>
         <label>
           Name:
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <button className='button' onClick={this.handleClick}>Click Me</button>
         <div
-          className="ag-theme-alpine"
-          style={{
-          height: '250px',
-          width: '600px' }}
-          >
-        <AgGridReact
-          columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}>
-        </AgGridReact>
+            className="ag-theme-alpine"
+            style={{
+            height: '250px',
+            width: '600px' }}
+            >
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}>
+          </AgGridReact>
+        </div>
+        <button
+            onClick={(e) => this.handleItemClick(e, 'sign-in')}
+        >
+          sign-in
+        </button>
+        <button
+            onClick={(e) => this.handleItemClick(e, 'sign-out')}
+        >
+          sign-out
+        </button>
       </div>
-      </form>
     )
   }
 }
